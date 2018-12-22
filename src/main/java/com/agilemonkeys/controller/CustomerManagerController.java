7b824bc685d0f33afa47caf0b75110bc88c1e0dc
Rodrigo <rdoria1@gmail.com>
2018-12-22 12:35:42 -0300

    Adapt CustomerManagerController and CustomerRepositoryImpl based on new domain layers and new Service.

--- src/main/java/com/agilemonkeys/controller/CustomerManagerController.java
+++ src/main/java/com/agilemonkeys/controller/CustomerManagerController.java
@@ -1,22 +1,27 @@
 package com.agilemonkeys.controller;
 
-import com.agilemonkeys.domain.Customer;
-import com.agilemonkeys.repository.CustomerRepository;
+import com.agilemonkeys.controller.RequestBody.CustomerRequestBody;
+import com.agilemonkeys.controller.ResponseBody.CustomerResponseBody;
+import com.agilemonkeys.service.CustomerManagerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class CustomerManagerController {
 
-    private CustomerRepository customerRepository;
+    private static final Logger log = LoggerFactory.getLogger(CustomerManagerController.class);
 
-    public CustomerManagerController(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
+    private CustomerManagerService customerManagerService;
+
+    public CustomerManagerController(CustomerManagerService customerManagerService) {
+        this.customerManagerService = customerManagerService;
     }
 
     @PostMapping(
@@ -24,15 +29,17 @@ public class CustomerManagerController {
         produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE}
     )
-    public ResponseEntity addCustomer(@RequestPart("customer") Customer customer, @RequestPart("image") MultipartFile image) {
-        customerRepository.addCustomer(customer);
-        System.out.println("image size" + image.getSize());
+    public ResponseEntity addCustomer(
+            @Valid @RequestPart("customer") CustomerRequestBody customerRequestBody,
+            @RequestPart("photo") MultipartFile photo) {
+        log.info("Adding new Customer {} with profile picture {}", customerRequestBody, photo.getName());
+        customerManagerService.addCustomer(customerRequestBody, photo);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/customers", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Customer>> getCostumers() {
-        List<Customer> costumers = customerRepository.getCustomers();
-        return ResponseEntity.ok(costumers);
+    public ResponseEntity<List<CustomerResponseBody>> getCostumers() {
+        log.info("Retrieving all customers...");
+        return ResponseEntity.ok(customerManagerService.getCustomers());
     }
 }
--- src/main/java/com/agilemonkeys/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/agilemonkeys/repository/CustomerRepositoryImpl.java
@@ -1,14 +1,19 @@
 package com.agilemonkeys.repository;
 
 import com.agilemonkeys.domain.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @Qualifier("fakeRepository")
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomerRepositoryImpl.class);
 
     private List<Customer> customers = new ArrayList<Customer>();
 
@@ -17,6 +22,10 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     }
 
     public void addCustomer(Customer customer) {
+        String uniqueID = UUID.randomUUID().toString();
+        customer.setId(uniqueID);
+        customer.setPhotoUrl("gcs://...");
         customers.add(customer);
+        log.info("Added new Customer {}", customer);
     }
 }
