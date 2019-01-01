2fcdf844d879cef6d5cec30c30ccb764602306a4
Rodrigo <rdoria1@gmail.com>
2018-12-31 20:57:47 -0400

    Added a new update operation for UserRepository: Custom operation was implemented because we are not supposed to update the password (since it's auto-generated). If password needs to be reseted, another operation must be implemented.

--- src/main/java/com/agilemonkeys/repository/UserRepository.java
+++ src/main/java/com/agilemonkeys/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.agilemonkeys.repository;
 
+import com.agilemonkeys.domain.Role;
 import com.agilemonkeys.domain.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,11 @@ public interface UserRepository extends CrudRepository<User, String> {
     @Transactional
     @Query("DELETE FROM User u WHERE u.username=:username")
     int deleteByUsername(@Param("username") String username);
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.name=:name, u.surname=:surname, u.role=:role WHERE u.username=:username")
+    int update(@Param("name") String name,
+                @Param("surname") String surname,
+                @Param("role") Role role,
+                @Param("username") String username);
 }
