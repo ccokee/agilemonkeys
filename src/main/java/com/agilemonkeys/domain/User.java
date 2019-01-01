8ec42420fea7f0afe6a42eca1f1da718a5f759ff
Rodrigo <rdoria1@gmail.com>
2018-12-31 20:55:55 -0400

    Added new Validation constraints based on groups.

--- src/main/java/com/agilemonkeys/domain/User.java
+++ src/main/java/com/agilemonkeys/domain/User.java
@@ -1,9 +1,11 @@
 package com.agilemonkeys.domain;
 
+import com.agilemonkeys.controller.UserValidationGroup;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.domain.Persistable;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -12,6 +14,9 @@ import javax.persistence.Column;
 import javax.persistence.Enumerated;
 import javax.persistence.EnumType;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 
 @Builder
 @NoArgsConstructor
@@ -22,9 +27,13 @@ import javax.persistence.Transient;
 public class User implements Persistable<String>{
 
     @Id
+    @NotBlank(groups = {UserValidationGroup.Add.class, UserValidationGroup.Update.class},
+              message = "Username must contain at least one non-whitespace character.")
+    @Length(min = 5, max = 32, groups = UserValidationGroup.Add.class, message = "Username must have between 5 and 32 characters.")
     String username;
 
     @Column(name = "password")
+    @Null(groups = {UserValidationGroup.Add.class, UserValidationGroup.Update.class}, message = "Password is auto-generated.")
     String password;
 
     @Column(name = "name")
@@ -35,10 +44,11 @@ public class User implements Persistable<String>{
 
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
+    @NotNull(groups = {UserValidationGroup.Add.class, UserValidationGroup.Update.class}, message = "Role must be provided.")
     Role role;
 
     @Transient
-    boolean isNew = false;
+    boolean isNew;
 
     @Override
     public String getId() {
