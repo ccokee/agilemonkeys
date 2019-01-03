bab978dc59591fa1acd291f476f6aaa92128b352
Rodrigo <rdoria1@gmail.com>
2019-01-03 15:51:31 -0400

    Change ExceptionResponse to ResponseBody generic class

--- src/main/java/com/agilemonkeys/controller/response/error/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/agilemonkeys/controller/response/error/CustomResponseEntityExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.agilemonkeys.controller.response.error;
 
-import com.agilemonkeys.exception.*;
+import com.agilemonkeys.controller.response.body.ResponseBody;
+import com.agilemonkeys.exception.CustomerNotFoundException;
+import com.agilemonkeys.exception.PhotoFileException;
+import com.agilemonkeys.exception.UserNotFoundException;
+import com.agilemonkeys.exception.UserAlreadyExistsException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,32 +23,32 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+        ResponseBody exceptionResponse = new ResponseBody(new Date(),
                 ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = PhotoFileException.class)
     protected ResponseEntity<Object> handleConflictPhotoFileException(PhotoFileException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage());
+        ResponseBody exceptionResponse = new ResponseBody(new Date(), ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = CustomerNotFoundException.class)
     protected ResponseEntity<Object> handleConflictCustomerNotFoundException(CustomerNotFoundException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage());
+        ResponseBody exceptionResponse = new ResponseBody(new Date(), ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = UserNotFoundException.class)
     protected ResponseEntity<Object> handleConflictUserNotFoundException(UserNotFoundException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage());
+        ResponseBody exceptionResponse = new ResponseBody(new Date(), ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = UserAlreadyExistsException.class)
     protected ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage());
+        ResponseBody exceptionResponse = new ResponseBody(new Date(), ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.CONFLICT);
     }
 }
